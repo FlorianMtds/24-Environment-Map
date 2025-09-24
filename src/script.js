79b1035c492c18f6a2fe86f230caeaeb8dec84eb
Florian <florianmtddev@gmail.com>
2025-09-24 13:56:12 +0200

    added gui tweaks

--- src/script.js
+++ src/script.js
@@ -24,6 +24,17 @@ const scene = new THREE.Scene()
 /**
  * Environment map
  */
+scene.environmentIntensity = 4
+scene.backgroundBlurriness = 0
+scene.backgroundIntensity = 5
+// scene.backgroundRotation.x = 1
+// scene.environmentRotation.x = 1
+
+gui.add(scene, 'environmentIntensity').min(0).max(10).step(0.001)
+gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001)
+gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
+gui.add(scene.backgroundRotation, 'y').min(0).max(Math.PI * 2).step(0.001)
+
 // LDR Cube Texture
 const environmentMap = cubeTextureLoader.load([
     '/environmentMaps/0/px.png',
